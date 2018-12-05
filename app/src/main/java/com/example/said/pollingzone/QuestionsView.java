ab2bb950f0713adc7d13af5736e8c81103b4dc24
Adam <adamlahjouji@rocektmail.com>
2018-12-05 13:09:46 -0500

    Added answer choice gui

--- app/src/main/java/com/example/said/pollingzone/QuestionsView.java
+++ app/src/main/java/com/example/said/pollingzone/QuestionsView.java
@@ -1,36 +1,100 @@
 package com.example.said.pollingzone;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class QuestionsView extends AppCompatActivity {
+
+    private ArrayList<String> mAnswerChoices = new ArrayList<>();
+    private ArrayList<String> mImagesUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer_questions);
-        Log.d(AppConsts.TAG, "Questions Activity");
+
 
         pressedSubmit();
+        initImageBitmaps();
     }
     //Todo: press submit adds the new question on to the screen and displays if user got the question right or wrong?
     private void pressedSubmit()
     {
 
+    }
+
+    private void initImageBitmaps(){
+        String []Letters = new String[16];
+        Letters[0] = "https://cdn.pixabay.com/photo/2017/02/09/11/46/alphabet-2051722__340.png";
+        Letters[1] = "https://cdn.pixabay.com/photo/2017/02/09/11/45/alphabet-2051720__340.png";
+        Letters[2] = "https://cdn.pixabay.com/photo/2017/02/09/11/43/alphabet-2051715__340.png";
+        Letters[3] = "https://cdn.pixabay.com/photo/2017/02/09/11/42/alphabet-2051714__340.png";
+        Letters[4] = "https://cdn.pixabay.com/photo/2017/02/09/11/40/alphabet-2051710__340.png";
+        Letters[5]= "https://cdn.pixabay.com/photo/2017/02/09/11/38/alphabet-2051709__340.png";
+        Letters[6] = "https://cdn.pixabay.com/photo/2017/02/09/11/37/alphabet-2051708__340.png";
+        Letters[7] = "https://cdn.pixabay.com/photo/2017/02/09/11/35/alphabet-2051707__340.png";
+        Letters[8] = "https://cdn.pixabay.com/photo/2017/02/09/11/33/alphabet-2051706__340.png";
+        Letters[9] = "https://cdn.pixabay.com/photo/2017/02/09/11/32/alphabet-2051705__340.png";
+        Letters[10] = "https://cdn.pixabay.com/photo/2017/02/09/11/31/alphabet-2051703__340.png"; // K
+        Letters[11] = "https://cdn.pixabay.com/photo/2017/02/09/11/29/alphabet-2051690__340.png";
+        Letters[12] = "https://cdn.pixabay.com/photo/2017/02/09/11/28/alphabet-2051689__340.png";
+        Letters[13] = "https://cdn.pixabay.com/photo/2017/02/09/11/26/alphabet-2051688__340.png";
+        Letters[14] = "https://cdn.pixabay.com/photo/2017/02/09/11/25/alphabet-2051685__340.png";
+        Letters[15] = "https://cdn.pixabay.com/photo/2017/02/09/11/23/alphabet-2051683__340.png";
+
+
+        //Todo: for loop, for every answer choice, i = 0, i < answerchoices, read from json and add it to mAnswerChoices, and do mImagesUrls.add(Letters[i]);
+        mImagesUrls.add(Letters[0]);
+        mAnswerChoices.add("Answer Choice A");
+
+        mImagesUrls.add(Letters[1]);
+        mAnswerChoices.add("Answer Choice B");
+
+        mImagesUrls.add(Letters[2]);
+        mAnswerChoices.add("Answer Choice C");
+
+        mImagesUrls.add(Letters[3]);
+        mAnswerChoices.add("Answer Choice D");
+
+        mImagesUrls.add(Letters[4]);
+        mAnswerChoices.add("Answer Choice E");
+
+        mImagesUrls.add(Letters[5]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[6]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[7]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[8]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[9]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[10]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[11]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[12]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[13]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[14]);
+        mAnswerChoices.add("Answer Choice E");
+        mImagesUrls.add(Letters[15]);
+        mAnswerChoices.add("Answer Choice E");
+
+
+        initRecyclerView();
+
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mImagesUrls, mAnswerChoices, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/said/pollingzone/RecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package com.example.said.pollingzone;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private static final String TAG = "RecyclerViewAdapter";
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContent;
+
+    public RecyclerViewAdapter(ArrayList<String> mImages, ArrayList<String> mImagesNames, Context mContent) {
+        this.mImages = mImages;
+        this.mContent = mContent;
+        this.mImageNames = mImagesNames;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_listitem, viewGroup, false );
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
+        Log.d(TAG, "onBindViewHolder: called");
+
+        Glide.with(mContent).asBitmap().load(mImages.get(i)).into(viewHolder.image);
+
+        viewHolder.imageName.setText(mImageNames.get(i));
+
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"onClick: clicked on: " + mImageNames.get(i));
+
+                Toast.makeText(mContent, mImageNames.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+
+    }
+}
